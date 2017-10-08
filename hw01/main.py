2d94d8e755380fca32f323605373c990a745ce2a
speedtestlong <451784355@qq.com>
2017-10-08 20:22:24 +0800

    Update main.py

--- hw01/main.py
+++ hw01/main.py
@@ -1,4 +1,6 @@
 for i in range(1, 10):
-        for j in range(1, i+1):
+        for j in range(1, i):
+            print('\t', end='')
+        for j in range(i, 10):
             print('{}x{}={}\t'.format(i, j, i*j), end='')
         print()
