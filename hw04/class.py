1dfb2bf640636a18e9bef034f20231a49fd86057
speedtestlong <451784355@qq.com>
2017-10-08 21:33:18 +0800

    Create class.py

---
+++ hw04/class.py
@@ -0,0 +1,13 @@
+def Ellipse(Ellipse,a,b):
+    Ellipse.a = a
+    Ellipse.b = b
+
+def Square(Square,a):
+    Square.a = a
+
+def Rectangle(Rectangle,a,b):
+    Rectangle.a = a
+    Rectangle.b = b
+
+def Circle(Circle,r):
+    Circle.r = r
